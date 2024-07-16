4f44e3d2a0bef25efdf4624d423e844563a01070
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-16 19:54:26 -0300

    white list

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // this will remove any property that is not defined in the DTO
+      forbidNonWhitelisted: true, // this will throw an error if any property is not defined in the DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
